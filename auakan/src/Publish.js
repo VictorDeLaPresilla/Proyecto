80fe8100b223802abd55701478cce5bcbae1c8b7
VictorDeLaPresilla <78715249+VictorDeLaPresilla@users.noreply.github.com>
2023-04-25 20:02:50 +0000

    responsive_done

--- auakan/src/Publish.js
+++ auakan/src/Publish.js
@@ -1,10 +1,10 @@
-
+import styles from "./Publish.module.css"
 import { Publishcard } from "./Publishcard";
 import publics from "./publi.json";
 export function Publish() {
     //console.log(publicaciones);
     return (
-        <ul>
+        <ul className={styles.publishgrid}>
            {publics.map((publica) => (
                 //<li key = {publica.id}>{publica.title}</li>
                 <Publishcard key = {publica.id} publica={publica}/>
--- auakan/src/Publishcard.js
+++ auakan/src/Publishcard.js
@@ -3,8 +3,12 @@ export function Publishcard({ publica }) {
     const imageurl = "https://image.tmdb.org/t/p/w300" + publica.poster_path;
     return (
        <li className={styles.publishcard}>
-            <img className={styles.publishimage} src = {imageurl} alt = {publica.title}/>
-
+            <img
+            width={230}
+            height={345} 
+            className={styles.publishimage}
+            src = {imageurl} 
+            alt = {publica.title}/>
             <div>{publica.title}</div>
         </li>
     );
